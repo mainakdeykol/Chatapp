ffaa87cd5dc292ae07851baa3180608ac3041c08
Mainak Dey <mainakdeykol@gmail.com>
2023-12-18 10:22:49 +0530

    chat

---
+++ client/src/RegisterAndLoginForm.jsx
@@ -0,0 +1,64 @@
+import {useContext, useState} from "react";
+import axios from "axios";
+import {UserContext} from "./UserContext.jsx";
+
+export default function RegisterAndLoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoginOrRegister, setIsLoginOrRegister] = useState('login');
+  const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+    const url = isLoginOrRegister === 'register' ? 'register' : 'login';
+    const {data} = await axios.post(url, {username,password});
+    setLoggedInUsername(username);
+    setId(data.id);
+  }
+  return (
+    <div className="bg-fuchsia-600 h-screen flex items-center">
+      <form className="w-64 mx-auto mb-12" onSubmit={handleSubmit}>
+      <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Username
+      </label>
+      <input value={username}
+               onChange={ev => setUsername(ev.target.value)}
+               type="text" placeholder="Enter Username"
+               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      </div>
+      <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+        Password
+      </label>
+      <input value={password}
+               onChange={ev => setPassword(ev.target.value)}
+               type="password"
+               placeholder="Enter Password"
+               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" />
+      </div>
+        
+        <button className="bg-pink-500 hover:bg-white-700 border-white-400 text-white font-bold py-2 px-4 rounded">
+          {isLoginOrRegister === 'register' ? 'Register' : 'Login'}
+        </button>
+        <div className="text-center mt-2">
+          {isLoginOrRegister === 'register' && (
+            <div>
+              Already a member?
+              <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow" onClick={() => setIsLoginOrRegister('login')}>
+                Login here
+              </button>
+            </div>
+          )}
+          {isLoginOrRegister === 'login' && (
+            <div>
+              Dont have an account?
+              <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => setIsLoginOrRegister('register')}>
+                Register
+              </button>
+            </div>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
